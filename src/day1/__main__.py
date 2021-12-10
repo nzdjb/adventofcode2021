b2b199eda217eecf229e7a20d56ba6f13ca8043a
nzdjb <github@2a1b.com>
2021-12-10 08:41:22 +0000

    feat: d1p2.

--- src/day1/__main__.py
+++ src/day1/__main__.py
@@ -8,7 +8,8 @@ def do(input_file):
     return len([True for i in range(1, len(lines)) if lines[i] > lines[i-1]])
 
 def do2(input_file):
-    return input_file
+    lines = read_to_ints(input_file)
+    return len([True for i in range(1, len(lines)-2) if sum(lines[i:i+2]) > sum(lines[i-1:i+1])])
 
 if __name__ == "__main__":
     print(do(argv[1]))

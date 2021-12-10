093eb42ba24237644944dc3310cf7f682d5df12e
nzdjb <github@2a1b.com>
2021-12-10 08:39:36 +0000

    feat: Further improve d1p1.

--- src/day1/__main__.py
+++ src/day1/__main__.py
@@ -5,10 +5,9 @@ from ..util import *
 
 def do(input_file):
     lines = read_to_ints(input_file)
-    return len([True for i in range(1, len(lines)) if int(lines[i]) > int(lines[i-1])])
+    return len([True for i in range(1, len(lines)) if lines[i] > lines[i-1]])
 
 def do2(input_file):
-    lines = read_to_ints(input_file)
     return input_file
 
 if __name__ == "__main__":

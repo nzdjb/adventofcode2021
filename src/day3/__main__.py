1366bc1bbd5e00afddfcebe6c6d42ab315a565d3
nzdjb <github@2a1b.com>
2021-12-10 08:22:35 +0000

    feat: Initial commit of days 1-8 before I redo them...

---
+++ src/day3/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from sys import argv
+from ..util import *
+
+def do(input_file):
+    return input_file
+
+def do2(input_file):
+    return input_file
+
+if __name__ == "__main__":
+    print(do(argv[1]))
+    print(do2(argv[1]))

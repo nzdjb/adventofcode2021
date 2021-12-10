fc5be5aa177801b5b0e5531d63df4e86d226918f
nzdjb <github@2a1b.com>
2021-12-10 08:35:00 +0000

    feat: Day 1 Part 1

--- src/day1/__main__.py
+++ src/day1/__main__.py
@@ -4,7 +4,8 @@ from sys import argv
 from ..util import *
 
 def do(input_file):
-    return input_file
+    lines = read_to_ints(input_file)
+    return len([True for i in range(len(lines)) if lines[i] != 0 and int(lines[i]) > int(lines[i-1])])
 
 def do2(input_file):
     return input_file

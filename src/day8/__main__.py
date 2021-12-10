8de84cd5e3886affacd31618769c19f1404423c2
nzdjb <github@2a1b.com>
2021-12-10 18:12:02 +0000

    feat(d8p1): Implementation.

--- src/day8/__main__.py
+++ src/day8/__main__.py
@@ -4,10 +4,20 @@ from sys import argv
 from ..util import *
 
 def do(input_file):
-    return input_file
+    lines = read_to_strings(input_file)
+    total = 0
+    for line in lines:
+        _, output = parse_line(line)
+        total += sum([len(digit) in [2, 3, 4, 7] for digit in output])
+    return total
 
 def do2(input_file):
+    lines = read_to_strings(input_file)
     return input_file
+
+def parse_line(line):
+    (input, output) = [io.split(' ') for io in line.strip().split(' | ')]
+    return (input, output)
 
 if __name__ == "__main__":
     print(do(argv[1]))

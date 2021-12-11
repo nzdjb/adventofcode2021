010fdaa417fd2a07271af76c1155e118aaa740ab
nzdjb <github@2a1b.com>
2021-12-11 01:22:21 +0000

    feat(d2p1): Implementation.

--- src/day2/__main__.py
+++ src/day2/__main__.py
@@ -4,9 +4,21 @@ from sys import argv
 from ..util import *
 
 def do(input_file):
-    return input_file
+    input = read_to_strings(input_file)
+    depth, distance = 0, 0
+    for line in input:
+        direction, amount = line.strip().split(' ')
+        match direction:
+            case 'forward':
+                distance += int(amount)
+            case 'up':
+                depth -= int(amount)
+            case 'down':
+                depth += int(amount)
+    return depth * distance
 
 def do2(input_file):
+    input = read_to_strings(input_file)
     return input_file
 
 if __name__ == "__main__":

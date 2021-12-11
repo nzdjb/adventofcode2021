f776daf3ff9ac0b00f4aa46593b2656914057e22
nzdjb <github@2a1b.com>
2021-12-11 01:35:12 +0000

    feat(d3p1): Implementation.

--- src/day3/__main__.py
+++ src/day3/__main__.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 from sys import argv
+from numpy import array
+from statistics import mode
 from ..util import *
 
 def do(input_file):
-    return input_file
+    input = array(read_to_int_lists(input_file))
+    most_common = int(''.join([str(mode(arr)) for arr in input.T]), 2)
+    least_common = int(''.join([str((mode(arr) - 1) * -1) for arr in input.T]), 2)
+    return most_common * least_common
 
 def do2(input_file):
+    input = read_to_int_lists(input_file)
     return input_file
 
 if __name__ == "__main__":
